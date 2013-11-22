d2c7d7148d83647df0741d34e8b163f2051e50db
paulmilligan57 <Paul.Milligan@edu.sait.ca>
2013-11-21 19:34:47 -0700

    payment.php added documentation

--- payment.php
+++ payment.php
@@ -4,7 +4,10 @@
 	Date: Nov 21, 2013
 	
 	payment.php
-	Booking page for customer and agent to book the travel packages
+	This page is arrived at after one of the following:
+      -a logged on agent or customer clicks an order button
+	  -a new customer clicks an order button then successfully registers in register.php
+    This page collects booking information then inserts a row into the bookings table of the database.
 -->
 <!DOCTYPE html>
 <?php
@@ -21,13 +24,14 @@
 				include_once("header.php");	
 			?>
 			<div id="content"> 
-			<form method="POST">
-				<table>
+			<form method="POST"> <!-- form that collects booking information -->
+				<table> <!-- table that aligns booking information -->
 			<?php
 				/*
-					Set initial variables and array to get the information from form
+					This page is reloaded when its form is submitted.
+					Initialize booking information to empty strings or the values submitted in the form.
 				*/
-				$today=date("Y-m-d");
+				$today=date("Y-m-d"); // get today's date
 			
 				$bookings=array();
 				$bookings['BookingDate']=isset($_POST["bdate"])? $_POST["bdate"]:$today;
@@ -37,39 +41,41 @@
 				$bookings['TripTypeId']=isset($_POST["ttype"])? $_POST["ttype"]:"";
 				$bookings['PackageId']=isset($_POST["package"])? $_POST["package"]:"";
 				
-				$submitFeedback="";
+				$submitFeedback=""; // will be set if there is feedback
 				
 				//connect to database
 				$conn=mysql_connect("localhost","root","");
 				$db=mysql_select_db('travelexperts') or die("Could not connect");
 				
-				//if login is customer
+				//if customer is logged in
 				if (isset($_SESSION['login']) && $_SESSION['login']=='customer') {
-					//if customer id is get
-					if (isset($_SESSION['customerId'])) { 
+					  // get customer ID from login or registration
+					if (isset($_SESSION['customerId'])) {
 						$bookings['CustomerId']=$_SESSION['customerId'];
-						
+						// get package ID from when order button was clicked
 						if (isset($_SESSION['order'])) {
 							$bookings['PackageId']=$_SESSION['order'];
 						}
 					}
-					else echo "No customer ID available for booking";
-				}
+				} // if agent is logged in
 				else if (isset($_SESSION['login']) && $_SESSION['login']=='agent'){
 					if (isset($_SESSION['order'])) {
+						// get package ID from when order button was clicked
 						$bookings['PackageId']=$_SESSION['order'];
 					}
+					// agent must select a customer, so show customer list
 			?>
 				<tr>
 					<td><p>Customer Name:</p></td>
 					<td>
 						<select name="cname" value="<?php echo $bookings['CustomerId'];?>"> 
 							<?php
-								//Select SQL statement for showing the customer
+								//Select SQL statement for listing the customers
 								$sql="SELECT CustomerId, CustFirstName, CustLastName FROM customers;";
 								$result=mysql_query($sql) or die(mysql_error());
 								while ($row=mysql_fetch_assoc($result)) {							
-									echo "<option value='".$row['CustomerId']."'>".$row['CustFirstName']." ".$row['CustLastName']."</option>";
+									echo "<option value='".$row['CustomerId']."'>";
+									echo $row['CustFirstName']." ".$row['CustLastName']."</option>";
 								}
 							?>
 						</select>
@@ -77,14 +83,13 @@
 				</tr>
 			<?php			
 					
-				}
-				else {
+				} else { // if neither customer nor agent logged in then go to home page
 					mysql_close($conn);	
 					header("Location: index.php");
 				}
 				
-				if (isset($_POST['submit'])) {
-					
+				if (isset($_POST['submit'])) { // if page was reloaded after clicking submit
+					// then insert the booking
 					$insertStr="INSERT INTO bookings (";
 					$valueStr=" VALUES (";
 					while (list($key, $value) = each($bookings)) {
@@ -102,16 +107,15 @@
 					
 					//insert sql statement 
 					$result=mysql_query($query_str) or die(mysql_error());
-					if ($result) 
+					if ($result) // set message to report what happened
 						$submitFeedback="Booking succeeded";
 					else
 						$submitFeedback="Booking failed";
 				}
 				
 			?>
-				
 					
-					<tr>
+					<tr> <!-- rest of form for booking information -->
 						<td>Date:</td>
 						<td><?=$bookings['BookingDate'];?></td>						
 					</tr>
@@ -154,23 +158,25 @@
 						<td> Package:</td>
 						<td>
 							
-							<?php
+							<?php // display the Package name but put package ID in the form
 								$sql="SELECT PkgName FROM packages where PackageId='".$bookings['PackageId']."';";
 								$result=mysql_query($sql) or die(mysql_error());
 								if ($result) {
 									$row=mysql_fetch_assoc($result);
-									//$bookings['PackageId']=$row[0];	
 									echo "<input type='hidden' name='package' value='".$bookings['PackageId'] ."' >".$row['PkgName'] ;
 								}
 							?>
 						</td>
 					</tr>
 					
-					<tr><td ><input type="submit" name="submit" value="Booking" /></td><td ><?=$submitFeedback;?></td></tr>
-					</table>			
-				
+					<tr>
+					  <td ><input type="submit" name="submit" value="Booking" /></td>
+					  <td ><?=$submitFeedback;?></td>
+					</tr>
+				  </table>			
 				</form>
-			</div>
+			</div> <!--  end of content div -->
+
 			<?php
 				mysql_close($conn);	
 				include_once("footer.php");
